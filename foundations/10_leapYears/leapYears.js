5a40f941397ded21891dad9ca40e06eaa8b0c4c8
Judá Teixeira <judateixeira>
2026-04-23 20:56:05 -0300

    Answer Foundations' exercise 11 of 16

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,11 +1,7 @@
 const leapYears = function(year) {
-  if (year % 100 === 0) {
-    if (year % 400 === 0) {
-      return true;
-    } else {
-      return false;
-    }
-  } else if (year % 4 === 0) {
+  if (year % 100 === 0 && year % 400 === 0) {
+    return true;
+  } else if (year % 100 != 0 && year % 4 === 0) {
       return true;
   } else {
     return false;
--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let result = (temp - 32) * (5/9);
+  return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let result = (temp * 9/5) + 32;
+  return Math.round(result * 10) / 10;
 };
+
+console.log(convertToCelsius(-100));
+console.log(convertToCelsius(32));
+console.log(convertToCelsius(100));
+
+
+console.log(convertToFahrenheit(-100));
+console.log(convertToFahrenheit(0));
+console.log(convertToFahrenheit(100));
+
 
 // Do not edit below this line
 module.exports = {
