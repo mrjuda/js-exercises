cbae74c79f6c4807e2b43da52841444f2b23fac1
Judá Teixeira <judateixeira>
2026-04-23 21:29:04 -0300

    Answer Foundations' exercise 12 of 16

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,70 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let arrayLength = array.length;
+  if (arrayLength === 0) {
+    return 0;
+  } else if (arrayLength === 1) {
+    return array[0];
+  } else {
+    let result = 0;
+    for (i = 0; i < arrayLength; i++) {
+      result += array[i];
+    }
+    return result;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let arrayLength = array.length;
+  if (arrayLength === 0) {
+    return 0;
+  } else if (arrayLength === 1) {
+    return array[0];
+  } else {
+    let result = array[0];
+    for (i = 1; i < arrayLength; i++) {
+      result *= array[i];
+    }
+    return result;
+  }
 };
 
 const power = function() {
-	
+	let exponent = arguments[1];
+  if (exponent === 0) {
+    return 1;
+  } else if (exponent === 1) {
+    return arguments[0];
+  } else {
+    let result = arguments[0];
+    for (i = 1; i < exponent; i++) {
+      result *= arguments[0];
+      console.log(result);
+    }
+    return result;
+  }
 };
 
-const factorial = function() {
-	
+console.log(power(4, 3)); // 64
+console.log(power(3, 10)); // 59049
+
+const factorial = function(number) {
+  let result = 1;
+  if (number === 0 || number === 1) {
+    return result;
+  } else {
+    for(i=2; i <= number; i++) {
+      result *= i;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
